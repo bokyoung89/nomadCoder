f20a80c1a0db033ffc375ed851980114541c6fbd
sbk8689 <sbk8689@gmail.com>
2020-03-10 12:07:12 +0900

    User Name Local storage 저장

--- gretting.js
+++ gretting.js
@@ -1,22 +1,38 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    gretting = document.querySelector("js-grettings");
+    greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
     SHOWING_CN ="showing";
 
-function paintGretting(text){
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+  }
+
+function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
-    gretting.classList.add(SHOWING_CN);
-    gretting.innerText = `hello ${text}`;
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
 }
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-        //she is not
+       askForName();
     } else{
-        paintGrettings(currentUser);
+        paintGreeting(currentUser);
     }
 }
 
